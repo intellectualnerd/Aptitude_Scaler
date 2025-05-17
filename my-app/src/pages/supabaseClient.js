77f547cff7d80f283d02e5985c2536797f57f01e
Devam <satasiyadevam@gmail.com>
2025-05-17 09:29:31 +0530

    env in gitingnore

--- my-app/src/pages/supabaseClient.js
+++ my-app/src/pages/supabaseClient.js
@@ -1,8 +1,7 @@
 // supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://xtbodvixmmejxnfcwllo.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0Ym9kdml4bW1lanhuZmN3bGxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNjQ0NzAsImV4cCI6MjA2MjY0MDQ3MH0.P6Kz6Dmso_shi21o6gIdbj73UCFTEFIieOtLhUErEME";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
