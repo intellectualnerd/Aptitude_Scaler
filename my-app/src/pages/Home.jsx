064c6860418e137b79b69964fadf6dfdba11c402
Bhavik2209 <bhavikrohit22@gmail.com>
2025-05-17 11:56:46 +0530

    update path

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from ".../utils/supabaseClient";
+import { supabase } from "../utils/supabaseClient";
 
 const Home = () => {
   const [profile, setProfile] = useState(null);
