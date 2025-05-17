bc20c5420f85ec658250f3768ed2e787683e581a
Parmar Parth <124517644+intellectualnerd@users.noreply.github.com>
2025-05-17 11:40:03 +0530

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,4 +1,4 @@
 // supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY);
+export const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.VITE_SUPABASE_ANON_KEY);
